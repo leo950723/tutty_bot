8200a986b0e3086cf1d0ef9949c9254439d392a8
leo950723 <leo950723op@gmail.com>
2023-07-25 14:47:03 +0800

    bot.py(update)

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 
 intents=discord.Intents.default()
 intents.members=True
-bot = commands.Bot(command_prefix='#',intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 async def on_ready():
@@ -18,4 +19,8 @@ async def on_member_remvoe(member):
     channel=bot.get_channel(1133086140681953412)
     await channel.send(f'{member} leave')
 
-bot.run("MTAxNzI0MjY4MDY4MzY2NzU0OA.GYufq1.lDz61KHQ9TmAJdl0z5JvMeEv4FbPZn3h8nS4Ao")
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+bot.run("MTAxNzI0MjY4MDY4MzY2NzU0OA.GTHl3Z.UH2YhGPDcDBbYnqXmWCDNDSI_9JIw5rHojMYSQ")
